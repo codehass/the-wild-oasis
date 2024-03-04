2b2264634dd9182b73c0dc845e7982e139d5cbdf
codehass <hassan.elouardy06@gmail.com>
2024-03-04 09:51:16 +0100

    duplicate cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -5,54 +5,52 @@ export async function getCabins() {
 
 	if (error) {
 		console.error(error);
-		throw new Error("Cabins can not be loaded");
+		throw new Error("Cabins could not be loaded");
 	}
 
 	return data;
 }
 
 export async function createEditCabin(newCabin, id) {
-	const hasImagePath = newCabin.image?.statWith?.(supabaseUrl);
+	const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+
 	const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
 		"/",
 		""
 	);
-
 	const imagePath = hasImagePath
 		? newCabin.image
 		: `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
-	//1. Create/edit cabins
+	// 1. Create/edit cabin
 	let query = supabase.from("cabins");
 
-	// A. CREATE
+	// A) CREATE
 	if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
 
-	// B EDIT
-	if (id)
-		query = query
-			.update({ ...newCabin, image: imagePath })
-			.eq("id", id)
-			.select();
+	// B) EDIT
+	if (id) query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
 
 	const { data, error } = await query.select().single();
 
 	if (error) {
 		console.error(error);
-		throw new Error("Cabins can not be created!");
+		throw new Error("Cabin could not be created");
 	}
-	//2. Upload image
 
-	const { error: StorageError } = await supabase.storage
+	// 2. Upload image
+	if (hasImagePath) return data;
+
+	const { error: storageError } = await supabase.storage
 		.from("cabin-images")
 		.upload(imageName, newCabin.image);
 
-	//3. Delete the cabin if There was an error uploading image
-	if (StorageError) {
-		supabase.from("cabins").delete().eq("id", data.id);
-		console.error(StorageError);
+	// 3. Delete the cabin IF there was an error uploading image
+	if (storageError) {
+		await supabase.from("cabins").delete().eq("id", data.id);
+		console.error(storageError);
 		throw new Error(
-			"Cabins image could not be uploaded! and the cabin was not created!"
+			"Cabin image could not be uploaded and the cabin was not created"
 		);
 	}
 
@@ -64,7 +62,7 @@ export async function deleteCabin(id) {
 
 	if (error) {
 		console.error(error);
-		throw new Error("Cabins can not be deleted!");
+		throw new Error("Cabin could not be deleted");
 	}
 
 	return data;
