8f53be84b65cfbff9ef101ec42014bea11c2991d
codehass <hassan.elouardy06@gmail.com>
2024-04-06 22:56:49 +0000

    add filtering

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -10,9 +10,11 @@ function CabinTable() {
 		queryKey: ["cabins"],
 		queryFn: getCabins,
 	});
-
 	const [searchParams] = useSearchParams();
 
+	if (isLoading) return <Spinner />;
+
+	// 1) Filter
 	const filterValue = searchParams.get("discount") || "all";
 
 	let filteredCabins;
@@ -22,7 +24,14 @@ function CabinTable() {
 	if (filterValue === "with-discount")
 		filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
-	if (isLoading) return <Spinner />;
+	// 2) SORT
+	const sortBy = searchParams.get("sortBy") || "startDate-asc";
+	const [field, direction] = sortBy.split("-");
+	const modifier = direction === "asc" ? 1 : -1;
+	const sortedCabins = filteredCabins.sort(
+		(a, b) => (a[field] - b[field]) * modifier
+	);
+
 	return (
 		<Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
 			<Table.Header>
@@ -35,7 +44,8 @@ function CabinTable() {
 			</Table.Header>
 			<Table.Body
 				// data={cabins}
-				data={filteredCabins}
+				// data={filteredCabins}
+				data={sortedCabins}
 				render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
 			/>
 		</Table>
