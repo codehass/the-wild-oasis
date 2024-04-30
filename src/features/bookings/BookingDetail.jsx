0c1a1c56c390e82a03be8816c0db10d63d17e63a
codehass <hassan.elouardy06@gmail.com>
2024-04-30 14:23:50 +0100

    checking out a booking and fix a bug

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -10,8 +10,9 @@ import ButtonText from "../../ui/ButtonText";
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
-import { HiArrowDownOnSquare } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiArrowUpOnSquare } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
 	display: flex;
@@ -21,6 +22,7 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
 	const { booking, isLoading } = useBooking();
+	const { checkout, isCheckingOut } = useCheckout();
 
 	const moveBack = useMoveBack();
 	const navigate = useNavigate();
@@ -56,6 +58,19 @@ function BookingDetail() {
 						Check in
 					</Button>
 				)}
+
+				{status === "checked-in" && (
+					<Button
+						icon={<HiArrowUpOnSquare />}
+						onClick={() => {
+							checkout(bookingId);
+						}}
+						disabled={isCheckingOut}
+					>
+						Check out
+					</Button>
+				)}
+
 				<Button variation="secondary" onClick={moveBack}>
 					Back
 				</Button>
