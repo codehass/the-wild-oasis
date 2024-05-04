7d2370d87ad2845d391e97ea2bd7e459625c9d01
codehass <hassan.elouardy06@gmail.com>
2024-05-04 14:32:30 +0100

    fixing an important bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,11 +10,10 @@ export function useLogin() {
 	const { mutate: login, isLoading } = useMutation({
 		mutationFn: ({ email, password }) => loginApi({ email, password }),
 		onSuccess: (user) => {
-			queryClient.setQueryData(["user"], user);
+			queryClient.setQueryData(["user"], user.user);
 			navigate("/dashboard", { replace: true });
 		},
 		onError: (err) => {
-			console.log("Error", err);
 			toast.error("Email or password provided are incorrect");
 		},
 	});
