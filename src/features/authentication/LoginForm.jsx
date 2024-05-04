f091d099645b7d71f90f24896df60e4e774e3ec7
codehass <hassan.elouardy06@gmail.com>
2024-05-04 14:21:24 +0100

    add user logout

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-	const [email, setEmail] = useState("hassan@example.com");
-	const [password, setPassword] = useState("hassan123");
+	const [email, setEmail] = useState("hassan@test.com");
+	const [password, setPassword] = useState("123456789");
 	const { login, isLoading } = useLogin();
 
 	function handleSubmit(e) {
