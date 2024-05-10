b80b120aaf869aced130b6f9f7ea8427ab9df839
codehass <hassan.elouardy06@gmail.com>
2024-05-10 21:44:01 +0100

    delete unwanted codes

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-	const [email, setEmail] = useState("hassan@test.com");
-	const [password, setPassword] = useState("123456789");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 	const { login, isLoading } = useLogin();
 
 	function handleSubmit(e) {
